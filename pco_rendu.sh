d13ca455302b95e3f34f7b86c1da7c96479aa3cb
Bruno Carvalho <bruno.darochacarvalho@heig-vd.ch>
2024-10-15 17:22:07 +0200

    Add script rendu

--- pco_rendu.sh
+++ pco_rendu.sh
@@ -2,7 +2,6 @@
 
 PROJECT_DIR=code
 REPORT_FILE=rapport.pdf
-PROJECT_FILE=Lab3_Factory.pro
 ARCHIVE=rendu.tar.gz
 
 if [ ! -d "$PROJECT_DIR" ]
@@ -17,11 +16,5 @@ then
     exit 1
 fi
 
-if [ ! -f "$PROJECT_DIR/$PROJECT_FILE" ]
-then
-    echo "Could not find project file : $PROJECT_FILE in $(realpath $PROJECT_DIR)" >&2
-    exit 1
-fi
-
 echo "The following files are archived in $ARCHIVE : "
-tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='*.user' -czvf $ARCHIVE code $REPORT_FILE
+tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='*.user' -czvf $ARCHIVE code/src code/ui code/images $REPORT_FILE
